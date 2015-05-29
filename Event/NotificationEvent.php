9138a38388fa0b55a7898233273110c7a029258d
Chabot Vincent <vince.chabot@gmail.com>
2015-05-29 10:47:36 +0200

    Change Event parameter
    From Entity\Notification to Model\NotificationInterface

--- Event/NotificationEvent.php
+++ Event/NotificationEvent.php
@@ -2,17 +2,22 @@
 
 namespace Joubjoub\NotificationBundle\Event;
 
-use Joubjoub\NotificationBundle\Entity\Notification;
+use Joubjoub\NotificationBundle\Model\NotificationInterface;
 use Symfony\Component\EventDispatcher\Event;
 
 class NotificationEvent extends Event
 {
 	/**
-	 * @var Notification
+	 * @var NotificationInterface
 	 */
 	protected $notification = null;
+	
+	public function __construct(NotificationInterface $notification)
+	{
+		$this->notification = $notification;
+	}
 
-	public function setNotification(Notification $notification)
+	public function setNotification(NotificationInterface $notification)
 	{
 		$this->notification = $notification;
 
